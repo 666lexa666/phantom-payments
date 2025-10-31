811d6342e17f985df67c72edac82b9561822e771
666Lexa666 <konoplevlesa5@gmail.com>
2025-10-31 06:42:11 +0300

    1/7

--- routes/operations/__init__.py
+++ routes/operations/__init__.py
@@ -1,9 +1,8 @@
 from flask import Blueprint
-from routes.api.operations.qr_code import qr_code_bp
-from routes.api.operations.qr_status import qr_status_bp
+from .qr_code import qr_code_bp
+from .qr_status import qr_status_bp
 
 operations_bp = Blueprint("operations", __name__)
 
-# Статические префиксы для вложенных blueprints
 operations_bp.register_blueprint(qr_code_bp, url_prefix="/qr-code")
 operations_bp.register_blueprint(qr_status_bp, url_prefix="/qr-status")
